ca08a7a2a23f52d361b25dc7db4b0b9d09340fda
Hamzaansari <hamzaansaree15@gmail.com>
2022-09-21 02:53:20 +0500

    Adding image

---
+++ app.js
@@ -0,0 +1,13 @@
+// let getData = () =>{
+//     let url = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=1e7e1d81c0c04b4f837e22b6e3f73046";
+// fetch(url).then((response) => {
+//     return response.json();
+// }).then((data) => {
+//     console.log(data);
+//     let firstLine = document.getElementById("first_line");
+//     let name = data["description"];
+//     firstLine.innerHTML = name
+
+// })
+// }
+// getData()
