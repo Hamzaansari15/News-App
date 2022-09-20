08ce2e512f6b0f0b3c31a5086fb7cb64c56b5b83
Hamzaansari <hamzaansaree15@gmail.com>
2022-09-21 01:15:00 +0500

    Create JS File

---
+++ News Website/app.js
@@ -0,0 +1,9 @@
+// let getData = () =>{
+//     let url = "https://newsapi.org/v2/top-headlines/sources?apiKey=1e7e1d81c0c04b4f837e22b6e3f73046";
+// fetch(url).then((response) => {
+//     return response.json();
+// }).then((data) => {
+//     console.log(data);
+// })
+// }
+// getData()
