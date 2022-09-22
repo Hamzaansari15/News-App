61d003a16a3deba1d8a4e18b71433559d745cc11
Hamzaansari <hamzaansaree15@gmail.com>
2022-09-23 03:07:06 +0500

    Update JS File

--- app.js
+++ app.js
@@ -76,3 +76,32 @@ let getDataBusiness = () => {
     })
 }
 getDataBusiness();
+
+
+// Business Completed
+
+let getDataHeadline = () => {
+    let url = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=1e7e1d81c0c04b4f837e22b6e3f73046"
+    fetch(url).then((response) => {
+        return response.json();
+    }).then((data) => {
+        for (let i = 0; i < 20; i++) {
+            let hamza =  `<div id="headline_card1">
+           <img src=${data.articles[i].urlToImage}>
+           <div id="headline_card1_text">
+              <p id="headline_card_title">${data.articles[i].title}</p>
+              <p id="headline_card_des">${data.articles[i].description}</p>
+              <p id="headline_card_publish">Published At: ${data.articles[i].publishedAt}</p>
+              <a href="${data.articles[i].url}" target="_blank"><button>Read more</button></a>
+           </div>
+           </div>`
+           let card = document.getElementById("headline_body");
+           card.innerHTML += hamza;
+ 
+        }
+    }).catch((Error) => {
+        console.log(Error);
+    })
+}
+getDataHeadline();
+
