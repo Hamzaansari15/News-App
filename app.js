94302bea63c69794f26d8925d02013381b7eac6c
Hamzaansari <hamzaansaree15@gmail.com>
2022-09-22 16:24:22 +0500

    Update JS File

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-// let getData = () =>{
-//     let url = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=1e7e1d81c0c04b4f837e22b6e3f73046";
-// fetch(url).then((response) => {
-//     return response.json();
-// }).then((data) => {
-//     console.log(data);
-//     for(let i = 0; i < data.articles.length; i++){
-//        console.log(data.articles[i]);  
-//     }
-// })
-// }
-// getData()
+let getData = () => {
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=1e7e1d81c0c04b4f837e22b6e3f73046"
+    fetch(url).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data.articles);
+        for (let i = 0; i < 20; i++) {
+           console.log(data.articles[i].title);
+           let hamza = document.getElementById("bitcoin_news");
+        //    hamza.innerHTML += data.articles[i].title;
+        }
+
+
+
+    }).catch((Error) => {
+        console.log(Error);
+    })
+}
+getData();
+
